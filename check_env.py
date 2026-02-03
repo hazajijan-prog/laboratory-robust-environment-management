7a25eda05fb81d5e93d651e1025412ec0c8c6e32
Haza Jijan <hazajijan@gmail.com>
2026-02-03 11:46:10 +0100

    Change litel in check

--- check_env.py
+++ check_env.py
@@ -1,13 +1,13 @@
 import sys 
 
 import pandas as pd 
-from sklearn import __version__ as sklearn_version
+import sklearn
 import torch
 
 def main (): 
     print("Python:", sys.version.split()[0])
     print("pandas:", pd.__version__)
-    print("scikit-learn:", sklearn_version)
+    print("scikit-learn:", sklearn.__version__)
     print("torch:", torch.__version__) 
 
 # Accelerator check
